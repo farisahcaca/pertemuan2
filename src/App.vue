e6bf257e6336c731a66eada2d9b5c8a71ff68365
farisahcaca <farisah8525@gmail.com>
2025-05-28 15:57:38 +0700

    pertemuan2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <DeclarativeRendering/>-->
+  <!-- <AttributeBinding/> -->
+  <!-- <FormBinding/> -->
+  <!-- <ConditionalRendering/> -->
+  <!-- <EventListeners/> -->
+  <!-- <ListRendering/> -->
+  <!-- <ComputedProperty/> -->
+  <!--  <Lifecycle/> -->
+   <Watchers/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import {ref} from 'vue'
+import DeclarativeRendering from './DeclarativeRendering.vue'
+import AttributeBinding from './AttributeBinding.vue'
+import EventListeners from './EventListeners.vue'
+import FormBinding from './FormBinding.vue'
+import ConditionalRendering from './ConditionalRendering.vue'
+import ListRendering from './ListRendering.vue' 
+import ComputedProperty from './ComputedProperty.vue'
+import Lifecycle from './components/Lifecycle.vue'  
+import Watchers from './components/Watchers.vue'
+
+</script>
+<stayle scoped></stayle>
